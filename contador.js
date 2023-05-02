683fdeca380fa1dafa2a61c2a4d9361db5ac7d5b
Mateus Stangherlin <mateus_stangherlin@hotmail.com>
2023-05-01 22:24:38 -0300

    Inserido contador de acessos

--- contador.js
+++ contador.js
@@ -1,6 +1,9 @@
 (function() {
-    let contador = localStorage.getItem("contador") || 0; 
-    contador = parseInt(contador) + 1;
-    localStorage.setItem("contador", contador); 
+    const isProprietario = window.location.search.includes("proprietario"); 
+    if (!isProprietario) { 
+      let contador = localStorage.getItem("contador") || 0;
+      contador = parseInt(contador) + 1;
+      localStorage.setItem("contador", contador);
+    }
   })();
   
